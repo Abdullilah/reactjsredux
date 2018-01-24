28cc01feb28f35723c5422d06e07818b2df4b9b5
abdullilaph <abdullilaphd@hotmail.com>
2018-01-24 16:24:41 +0100

    Outputting dynamic Data

--- src/app/Home.js
+++ src/app/Home.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class Home extends Component {
     render() {
+        let x = 'Hello';
+        if(true){
+            x += ' Max';
+        }
         return (
             <div>
                 <h1>Home</h1>
+                {x}
+                <br/>
+                {2 + 5}
             </div>
         );
     }
