201a323523b449ebaa94d4475f7407bf910f0cba
abdullilaph <abdullilaphd@hotmail.com>
2018-01-24 18:37:18 +0100

    Change class component to const if it does not contains state

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Header</h1>
-                <h2>{this.props.data.name}</h2>
-                <ul>
-                    {this.props.data.arr.map( (ele, i) => <li key={i}>{ele}</li>)}
-                </ul>
-                <h2>{this.props.children}</h2>
-            </div>
-        );
-    }
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <h2>{props.data.name}</h2>
+            <ul>
+                {props.data.arr.map( (ele, i) => <li key={i}>{ele}</li>)}
+            </ul>
+            <h2>{props.children}</h2>
+        </div>
+    );
 }
 
 export default Header;
