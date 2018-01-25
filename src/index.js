a00fd69f477f1fe3b70b6a924aea23bc61a16a5c
abdullilaph <abdullilaphd@hotmail.com>
2018-01-25 18:26:51 +0100

    Create Redux Store and first reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {createStore} from 'redux';
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case "ADD":
+            state += action.payload;
+            break;
+        case "Sub":
+            state -= action.payload;
+            break;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(()=> {
+    console.log(store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 10
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 5
+});
+
+store.dispatch({
+    type: "Sub",
+    payload: 10
+});
